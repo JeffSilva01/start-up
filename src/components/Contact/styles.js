f79664d4798a7e8d2e5efee4d4acf276131b9125
Jefferson Silva <jeff_silva01@yahoo.com>
2019-10-25 22:13:57 -0300

    Retornando commit

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 100vmax;
+  min-height: 100%;
   display: flex;
   justify-content: center;
 
